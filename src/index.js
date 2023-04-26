3f10ca5ba91aa71fa24d3e59bfa6fa6d5b57d806
mother426 <johnkimmsterr@gmail.com>
2023-04-26 12:30:56 -0400

    form slice

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const el = document.getElementById('root');
-const root = createRoot(el);
+const root = createRoot(document.getElementById('root'));
 
 root.render(<App />);
