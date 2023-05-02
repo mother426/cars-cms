88252cbaea687a76f0d01868bd2fd601d8bf1d35
mother426 <johnkimmsterr@gmail.com>
2023-05-02 16:25:40 -0400

    car deletion

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,11 +1,38 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 function CarList() {
+  const dispatch = useDispatch();
   const cars = useSelector((state) => {
     return state.cars.collection;
-  }); 
+  });
 
-  return <div>CarList</div>;
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
+  const carData = cars.map((car) => {
+    return (
+      <div key={car.id}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {carData}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
